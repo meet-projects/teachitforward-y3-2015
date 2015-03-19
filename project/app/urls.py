ff44b2415c4ed92baed67c02e9bd66e738a9e742
Guest <guest-gOJTWO@meet.mit.edu>
2015-03-19 19:52:16 +0200

    first push

--- project/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-
-from app import views
-
-urlpatterns = patterns('',
-    url(r'^home/$', views.home, name='home'),
-    url(r'^$', views.index, name='index'),
-)
