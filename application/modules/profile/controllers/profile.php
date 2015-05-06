806d686254f97e469c16bf1510be94f4bf43ff4f
Sumune <b@hjat.cu.cc>
2015-05-06 12:52:02 +0300

    .

--- application/modules/profile/controllers/profile.php
+++ application/modules/profile/controllers/profile.php
@@ -8,12 +8,12 @@ class profile extends MX_Controller {
     public function index() {
 		$this->load->model("profile_model");
 		
-		//$id = $this->input->get("id");
-		//if($this->input->get("id")==FALSE)
-			$id = this->session->userdata("ID");
+		$id = $this->input->get("id");
+		if($this->input->get("id")==FALSE)
+			$id = $this->session->userdata("ID");
 		
 		$details = $this->profile_model->getDetails($id);
-		//if($details==FALSE) return;
+		if($details==FALSE) return;
 		
 		$module = Modules::load('template');
 		$data = array(
